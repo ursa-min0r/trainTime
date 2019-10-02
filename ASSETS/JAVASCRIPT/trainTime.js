dc05a0bd881be7c1e1bb5eeaf731fd09d484e1ac
Polaris <tristynkcline@gmail.com>
2019-10-02 13:30:04 -0500

    Update HTML CSS JAVASCRIPT skeleton

---
+++ ASSETS/JAVASCRIPT/trainTime.js
@@ -0,0 +1,14 @@
+
+  var config = {
+    apiKey: "AIzaSyDIE9v8xit4FN0HEPip_E8CZqptyAcyECQ",
+    authDomain: "traintimehw-a8131.firebaseapp.com",
+    databaseURL: "https://traintimehw-a8131.firebaseio.com",
+    projectId: "traintimehw-a8131",
+    storageBucket: "",
+    messagingSenderId: "543968700635",
+    appId: "1:543968700635:web:f0de3c6868e8605f2504fe"
+  };
+
+  firebase.initializeApp(config);
+
+  
